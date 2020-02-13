f9ea23166dccb519ae08e8da2911dd8d1b5ce360
tarik-84 <niceloveman200@yahoo.com>
2020-02-13 01:39:53 -0500

    new file

--- js/index.js
+++ js/index.js
@@ -40,3 +40,112 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Nav
+
+let services = document.getElementsByTagName('a')[0];
+services.textContent = 'Services';
+
+let product = document.getElementsByTagName('a')[1];
+product.textContent = 'Product';
+
+let vision = document.getElementsByTagName('a')[2];
+vision.textContent = 'Vision';
+
+let features = document.getElementsByTagName('a')[3];
+features.textContent = 'Features';
+
+let about = document.getElementsByTagName('a')[4];
+about.textContent = 'About';
+
+let contact = document.getElementsByTagName('a')[5];
+contact.textContent = 'Contact';
+
+//cta
+
+let headerImg = document.getElementById('cta-img');
+headerImg.src = 'img/header-img.png';
+
+let h1 = document.getElementsByTagName('h1')[0];
+h1.textContent = 'DOM Is Awesome';
+
+let button = document.getElementsByTagName('button')[0];
+button.textContent = 'Get Started';
+
+
+//main-content
+
+let featuresH4 = document.getElementsByTagName('h4')[0];
+featuresH4.textContent = 'Features';
+
+let featuresContent = document.getElementsByTagName('p')[0];
+featuresContent.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+let aboutH4 = document.getElementsByTagName('h4')[1];
+aboutH4.textContent = 'About';
+
+let aboutContent = document.getElementsByTagName('p')[1];
+aboutContent.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+let middleImg = document.getElementById('middle-img');
+middleImg.src = 'img/mid-page-accent.jpg';
+
+let servicesH4 = document.getElementsByTagName('h4')[2];
+servicesH4.textContent = 'Services';
+
+let servicesContent = document.getElementsByTagName('p')[2];
+servicesContent.textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+
+let productH4 = document.getElementsByTagName('h4')[3];
+productH4.textContent = 'Product';
+
+let productContent = document.getElementsByTagName('p')[3];
+productContent.textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+
+
+let visionH4 = document.getElementsByTagName('h4')[4];
+visionH4.textContent = 'Vision';
+
+let visionContent = document.getElementsByTagName('p')[4];
+visionContent.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+//contact
+
+let contacth4 = document.getElementsByTagName('h4')[5];
+contacth4.textContent = 'Contact';
+
+let address = document.getElementsByTagName('p')[5];
+address.textContent = '123 Way 456 Street Somewhere, USA';
+
+let phone = document.getElementsByTagName('p')[6];
+phone.textContent = '1 (888) 888-8888';
+
+let email = document.getElementsByTagName('p')[7];
+email.textContent = 'sales@greatidea.io';
+
+
+//footer
+
+let footer = document.getElementsByTagName('p')[8];
+footer.textContent = 'Copyright Great Idea! 2018';
+
+
+
+let links = document.querySelectorAll('a');
+links.forEach(link => {
+  link.style.color = 'green';
+})
+
+
+
+let newA1 = document.createElement('a');
+newA1.textContent = 'Help';
+
+let newA2 = document.createElement('a');
+newA2.textContent = 'Home';
+
+let parentElement = document.querySelector('nav');
+parentElement.append(newA1);
+parentElement.prepend(newA2);
